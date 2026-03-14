b8713edb46663a6482cb93bfacf0b41e5fb7d2b2
selahvidallc <garrksmith@gmail.com>
2026-03-13 18:37:07 -0600

    Allow liarspokerclub domains in CORS

--- app/main.py
+++ app/main.py
@@ -19,6 +19,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://liarspokerclub-web.vercel.app",
+        "https://liarspokerclub.com",
+        "https://www.liarspokerclub.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
